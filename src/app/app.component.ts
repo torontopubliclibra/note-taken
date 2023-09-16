7e83e8a989254f3bc991fd0e6af45378fb695ee6
torontopubliclibra <dana.r.teagle@gmail.com>
2023-09-15 20:26:09 -0400

    refactor: upgraded pigeon pad into note taken

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,152 +10,263 @@ import { Component } from '@angular/core';
 // component class
 export class AppComponent {
 
-  // app title
-  title: string = 'pigeon-pad';
-
-  // default textbox value
-  textareaValue: string = '';
-
-  // default note color
-  noteColor: string = 'pink';
-
-  // default placeholder text
-  placeholder: string = "Add a note"
-  
-  // empty notes array
-  notes: {text: string, color: string}[] = [];
-
-  // default darkMode value
-  darkMode: boolean = false;
+  // initial variables
+  title: string = `note-taken`;
+  theme: string = `light`;
+  storedTheme: any = localStorage.getItem(`theme`);
+  localNotes: any = localStorage.getItem(`notes`);
+  allNotes: {
+    text: string,
+    color: string,
+    meta: {
+      time: string,
+      date: string,
+    }
+  }[] = [];
+  filteredNotes: {
+    text: string,
+    color: string,
+    meta: {
+      time: string,
+      date: string,
+    }
+  }[] = [];
+  placeholder: string = `Type your note here`;
+  noteColor: string = `green`;
+  colorFilter: string = `all`;
+  timeSort: string = `desc`;
+  noteColors: string[] = [];
+  inputtedText: string = ``;
 
   // component class constructor
   constructor() {
-
-    // get local storage notes value
-    let savedNotes: any = localStorage.getItem('notes');
-
-    // get local storage mode value
-    let savedMode: any = localStorage.getItem('mode');
    
-    // if savedNotes isn't null
-    if (savedNotes) {
-      // set notes to parsed array
-      this.notes = JSON.parse(savedNotes);
+    // if there are notes stored, set the notes to the parsed array
+    if (this.localNotes) {
+      this.allNotes = JSON.parse(this.localNotes);
 
+    // otherwise set the notes to an empty array
     } else {
-      // else set notes to an empty array
-      this.notes = [];
+      this.allNotes = [];
     }
 
-    // if savedMode is dark
-    if (savedMode === 'dark') {
-      // turn on dark mode
-      this.darkMode = true;
+    // if the stored theme is dark, turn on the dark theme
+    if (this.storedTheme === `dark`) {
+      this.theme = `dark`;
     }
+
+    this.filteredNotes = this.allNotes;
+
+    this.updateNoteColors();
   }
 
-  // change placeholder text function (takes new placeholder as argument)
-  changePlaceholderText = (newPlaceholder: string) => {
+  updatePlaceholder = (newPlaceholder: string) => {
 
-    // set placeholder to new placeholder
     this.placeholder = newPlaceholder;
 
-    // after 1 second, reset the placeholder
     setTimeout(() => {
-      this.placeholder = "Add a note"
+      this.placeholder = 'Type your note here';
     }, 1000)
   }
 
-  // save function
-  save() {
+  updateNoteColors = () => {
+    let newNoteColors: string[] = [];
 
-    // create copy of notes
-    let updateNotes: {text: string, color: string}[] = this.notes;
+    this.allNotes.forEach((note) => {
+      if (!newNoteColors.includes(note.color)) {
+        newNoteColors.push(note.color);
+      }
+    })
 
-    // if the notebook has 12 or more notes
-    if (updateNotes.length >= 12) {
-      // display limit text
-      this.changePlaceholderText("Notes limit exceeded");
+    if (!newNoteColors.includes(this.colorFilter)) {
+      this.colorFilter = `all`;
+      this.filterNotesByColor();
+    }
 
-    // if note is already in notebook
-    } else if (updateNotes.some(note => note.text === this.textareaValue)) {
-      // display duplicate text
-      this.changePlaceholderText("Already in notes");
+    this.noteColors = newNoteColors;
+
+  }
+
+  filterNotesByColor = () => {
+
+    let updatedNotes: {
+      text: string,
+      color: string,
+      meta: {
+        time: string,
+        date: string,
+      }
+    }[] = this.allNotes;
+
+    if (this.colorFilter !== `all`) {
+      updatedNotes = updatedNotes.filter(note => note.color === this.colorFilter);
+    }
+
+    this.filteredNotes = updatedNotes;
+
+  }
+
+  // save note function
+  saveNote(event: any) {
+
+    if (event.keyCode == 13) {
+      event.preventDefault();
+    }
+
+    let today = new Date();
+    let day = today.getDate();
+    let monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
+  "July", "Aug", "Sept", "Oct", "Nov", "Dec"];
+    let month = monthNames[today.getMonth()];
+    let year = String(today.getFullYear());
+    let hour = "";
+    let meridiem = "AM";
+    if (today.getHours() > 12) {
+      hour = String(today.getHours() - 12);
+      meridiem = 'PM'
+    } else {
+      hour = String(today.getHours());
+    }
+    let minute = String(today.getMinutes()).padStart(2, '0');
     
-    // if note is blank
-    } else if (!this.textareaValue || this.textareaValue === " ") {
-      // display empty note text
-      this.changePlaceholderText("Note can't be blank");
+    let noteTime: string = `${hour}:${minute} ${meridiem}`
+    let noteDate: string = `${month} ${day}, ${year}`
+
+    // create a copy of the current notes and placeholder
+    let updateNotes: {
+      text: string,
+      color: string,
+      meta: {
+        time: string,
+        date: string,
+      }
+    }[] = this.allNotes;
+    let initialPlaceholder = this.placeholder;
+
+    // if there are 20 or more notes, display the limit text
+    if (updateNotes.length >= 20) {
+      this.updatePlaceholder(`Storage limit reached`);
+
+    // if the note is already in the array, display the duplicate note text
+    } else if (updateNotes.some(note => note.text === this.inputtedText)) {
+      this.updatePlaceholder(`Note already exists`);
+    
+    // if the note is blank, display the empty note text
+    } else if (this.inputtedText === "" || this.inputtedText == "\n") {
+
+      this.inputtedText = ``;
+      this.updatePlaceholder(`Note can't be empty`);
 
     // otherwise
     } else {
-      // push textbox value to notes copy
-      updateNotes.push(
-        {
-          text: this.textareaValue,
+
+      // push the inputted text to the updated notes
+      updateNotes.push({
+          text: this.inputtedText,
           color: this.noteColor,
+          meta: {
+            time: noteTime,
+            date: noteDate,
+          }
         });
-      this.placeholder = "Add a note"
+
+      // and reset the placeholder
+      this.placeholder = initialPlaceholder;
+
     }
 
-    // set notes to updated notes copy
-    this.notes = updateNotes;
+    console.log(updateNotes);
 
-    // set local storage item to stringified array
-    localStorage.setItem('notes', JSON.stringify(updateNotes));
+    // set the notes to the updated notes
+    this.allNotes = updateNotes;
+
+    this.updateNoteColors();
+
+    if (this.colorFilter !== this.noteColor) {
+      this.colorFilter = `all`;
+    }
+
+    this.filterNotesByColor();
+
+    // set the local storage notes to the stringified array
+    localStorage.setItem(`notes`, JSON.stringify(updateNotes));
 
     // clear out textbox value
-    this.textareaValue = '';
+    this.inputtedText = ``;
+
   }
 
-  // remove function (takes array index)
-  remove(index: number) {
+  // edit note function
 
-    // create copy of notes
-    let updateNotes: {text: string, color: string}[] = this.notes;
+  editNote(index: number) {
 
-    // remove item from notes array copy at reverse index
-    updateNotes.splice(((updateNotes.length - 1) - index), 1);
+    let editedNote = this.filteredNotes[((this.filteredNotes.length - 1) - index)];
 
-    // set notes to updated notes copy
-    this.notes = updateNotes;
+    this.inputtedText = editedNote.text;
+    this.noteColor = editedNote.color;
 
-    // set local storage item to stringified array 
-    localStorage.setItem('notes', JSON.stringify(updateNotes));
+    this.removeNote(index);
+
+  }
+
+  // remove note function
+  removeNote(index: number) {
+
+    let removedNote = this.filteredNotes[((this.filteredNotes.length - 1) - index)];
+
+    this.filteredNotes = this.filteredNotes.filter(note => note.text !== removedNote.text);
+
+    this.allNotes = this.allNotes.filter(note => note.text !== removedNote.text);
+
+    this.localNotes = this.allNotes;
+
+    this.updateNoteColors();
+
+    // set the local storage notes to the stringified array 
+    localStorage.setItem(`notes`, JSON.stringify(this.localNotes));
+
   }
 
   // remove all notes function
   removeAll() {
 
-    // set notes to an empty array
-    this.notes = [];
+    // empty all the notes arrays
+    this.allNotes = [];
+    this.filteredNotes = [];
+    this.localNotes = [];
+    
+    this.updateNoteColors();
 
     // clear the local storage
-    localStorage.removeItem('notes');
+    localStorage.removeItem(`notes`);
+
   }
 
-  // clear function
-  clear() {
+  // clear text function
+  clearText() {
 
     // set textbox value to an empty string
-    this.textareaValue = "";
+    this.inputtedText = ``;
+
   }
 
-  // turn on dark mode function
-  turnOnDark() {
+  // update theme function
+  updateTheme() {
 
-    // switch on/off dark mode
-    this.darkMode = !this.darkMode;
+    // if theme is dark
+    if (this.theme === `dark`) {
 
-    // if darkMode is true
-    if (this.darkMode) {
+      // update theme to light and send to local storage
+      this.theme = `light`
+      localStorage.setItem(`theme`, `light`);
 
-      // save dark mode to local storage
-      localStorage.setItem('mode', 'dark');
+    // if theme is light
     } else {
 
-      // else save light mode to local storage
-      localStorage.setItem('mode', 'light');
+      // update theme to dark and send to local storage
+      this.theme = `dark`;
+      localStorage.setItem(`theme`, `dark`);
+
     }
   }
 
